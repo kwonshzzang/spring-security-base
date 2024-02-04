f2f0202874fda9dda60c0160ac3c1278fe0d5cf3
kwonshzzang <kwonshzzang@gmail.com>
2024-02-04 21:07:40 +0900

    First Commit

---
+++ server/login-basic/src/main/java/kr/co/kwonshzzang/LoginBasicApplication.java
@@ -0,0 +1,11 @@
+package kr.co.kwonshzzang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginBasicApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoginBasicApplication.class, args);
+    }
+}
